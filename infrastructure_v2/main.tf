5b56c2bb53ad69ac63f87346702731948f43577c
Phil Duncan <philipduncan860@gmail.com>
2026-07-27 12:37:18 -0400

    fix public IP mapping

--- infrastructure_v2/main.tf
+++ infrastructure_v2/main.tf
@@ -2550,7 +2550,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.lab.id
   cidr_block              = var.public_subnet_cidr
   availability_zone       = data.aws_availability_zones.available.names[0]
-  map_public_ip_on_launch = false
+  map_public_ip_on_launch = true
 
   tags = {
     Name        = "${var.environment_name}-public-subnet-a"

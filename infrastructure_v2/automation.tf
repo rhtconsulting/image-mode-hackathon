b85d7226152dfdfdccae52f1eed24bdf871e8030
Phil Duncan <philipduncan860@gmail.com>
2026-08-16 09:54:09 -0400

    switch branch

--- infrastructure_v2/automation.tf
+++ infrastructure_v2/automation.tf
@@ -272,7 +272,7 @@ resource "terraform_data" "bootstrap_lab" {
       INVENTORY_FILE="${abspath(path.module)}/inventory.ini"
 
       REPO_URL="https://github.com/claudiol/image-mode-hackathon.git"
-      BRANCH="dev"
+      BRANCH="deploy-rhtas-server"
 
       echo "Using inventory: $INVENTORY_FILE"
 

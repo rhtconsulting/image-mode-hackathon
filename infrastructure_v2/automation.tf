20a9866e56cc2b6fec37b61a56de058a142c1f30
Phil Duncan <philipduncan860@gmail.com>
2026-09-18 09:08:34 -0400

    switch branch

--- infrastructure_v2/automation.tf
+++ infrastructure_v2/automation.tf
@@ -375,7 +375,7 @@ resource "terraform_data" "bootstrap_lab" {
       INVENTORY_FILE="${abspath(path.module)}/inventory.ini"
 
       REPO_URL="https://github.com/rhtconsulting/image-mode-hackathon.git"
-      BRANCH="dev"
+      BRANCH="set-consistent-shell-for-aws"
 
       echo "Using inventory: $INVENTORY_FILE"
 

0ace00e320dd15fe044ce0e3beaa21d8584f3571
philip860 <52895298+philip860@users.noreply.github.com>
2026-09-21 10:35:35 -0400

    Update automation.tf

--- infrastructure_v2/automation.tf
+++ infrastructure_v2/automation.tf
@@ -375,7 +375,7 @@ resource "terraform_data" "bootstrap_lab" {
       INVENTORY_FILE="${abspath(path.module)}/inventory.ini"
 
       REPO_URL="https://github.com/rhtconsulting/image-mode-hackathon.git"
-      BRANCH="dev"
+      BRANCH="devv"
 
       echo "Using inventory: $INVENTORY_FILE"
 

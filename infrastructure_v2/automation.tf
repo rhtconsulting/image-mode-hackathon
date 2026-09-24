85524ea22840efdb4540ee4c183acc37c8fd59b7
Phil Duncan <philipduncan860@gmail.com>
2026-09-24 09:22:01 -0400

    switch branch

--- infrastructure_v2/automation.tf
+++ infrastructure_v2/automation.tf
@@ -376,7 +376,7 @@ resource "terraform_data" "bootstrap_lab" {
       DEPLOY_SUCCESS_FILE="${abspath(path.module)}/.deploy-services-success"
 
       REPO_URL="https://github.com/rhtconsulting/image-mode-hackathon.git"
-      BRANCH="dev"
+      BRANCH="adjust-secret-cleanup"
 
       echo "Using inventory: $INVENTORY_FILE"
 
